022e3dfb9a4d625cb624deb35e914374248b508d
Erivelton Oliveira <143474678+erivelton-oliveira@users.noreply.github.com>
2025-03-21 14:15:48 -0300

    Update main.py
    Alteração para teste

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ window = pygame.display.set_mode(size=(600, 480))
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print("Quiting...")
             pygame.quit()
             quit()
