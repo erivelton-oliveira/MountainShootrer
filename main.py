1f29cc315ca62767fe627c2d7d2da1629e4db412
Erivelton Oliveira <felipedeoliveira.02690@gmail.com>
2025-03-21 16:21:07 -0300

    Adding classes generated from StarUML

--- main.py
+++ main.py
@@ -6,6 +6,5 @@ window = pygame.display.set_mode(size=(600, 480))
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            print("Quiting...")
             pygame.quit()
             quit()
